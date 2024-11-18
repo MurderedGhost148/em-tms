e97215f2bc8bd8a2e7fad1eec1b6d3422052cef8
MurderedGhost148 <farm1invasion@gmail.com>
2024-11-18 13:51:22 +0400

    Переименованы классы запуска приложения и тестов.

---
+++ src/main/java/ru/em/tms/TMSApp.java
@@ -0,0 +1,13 @@
+package ru.em.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMSApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TMSApp.class, args);
+    }
+
+}
