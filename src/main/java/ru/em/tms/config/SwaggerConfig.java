41e3366b62b27f93dc0a3fdaebda1b13d581eeee
MurderedGhost148 <farm1invasion@gmail.com>
2024-11-22 12:55:58 +0400

    Изменено описание сервиса (swagger-ui).

--- src/main/java/ru/em/tms/config/SwaggerConfig.java
+++ src/main/java/ru/em/tms/config/SwaggerConfig.java
@@ -2,28 +2,42 @@ package ru.em.tms.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Task Management System")
+                .description("Описание API сервиса")
+                .version("1.0")
+                .contact(new Contact()
+                        .name("Денис Ёрочкин")
+                        .email("farm2invasion@gmail.com"));
+    }
+
     private SecurityScheme createAPIKeyScheme() {
         return new SecurityScheme().type(SecurityScheme.Type.HTTP)
                 .in(SecurityScheme.In.HEADER)
                 .name("Authorization")
                 .bearerFormat("JWT")
                 .scheme("bearer");
-    }
-
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().
-                        addList("Bearer Authentication"))
-                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
-                .info(new Info().title("Task Management System")
-                        .version("1.0"));
     }
 }
