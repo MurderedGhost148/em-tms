8fce9729728f95f2ede8f2853b0c8dcb3bab314e
MurderedGhost148 <farm1invasion@gmail.com>
2024-11-24 19:17:24 +0400

    Обновлен скрипт с начальными данными.

--- data/data.sql
+++ data/data.sql
@@ -1,10 +1,10 @@
 USE tms_db;
 
 INSERT INTO users (email, password, role) VALUES
-    ('admin1@test.ru', '$2a$10$rE8zT/MhlQlwRAFqoeSoAOXxnopkdVxXBJ/1Tgu8ws9dujqOsU1j.', 'ADMIN'),
-    ('user1@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER'),
-    ('user2@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER'),
-    ('user3@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER'),
-    ('admin2@test.ru', '$2a$10$rE8zT/MhlQlwRAFqoeSoAOXxnopkdVxXBJ/1Tgu8ws9dujqOsU1j.', 'ADMIN'),
-    ('admin3@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER');
+    ('admin1@test.ru', '$2a$10$rE8zT/MhlQlwRAFqoeSoAOXxnopkdVxXBJ/1Tgu8ws9dujqOsU1j.', 'ADMIN'), /* password = 87654321cxZ! */
+    ('user1@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER'), /* password = 12345zxC! */
+    ('user2@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER'), /* password = 12345zxC! */
+    ('user3@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER'), /* password = 12345zxC! */
+    ('admin2@test.ru', '$2a$10$rE8zT/MhlQlwRAFqoeSoAOXxnopkdVxXBJ/1Tgu8ws9dujqOsU1j.', 'ADMIN'), /* password = 87654321cxZ! */
+    ('user4@test.ru', '$2a$10$st6KaLrhWiFAw6mA3vSVseVDwAHCIJrOSyqvqs75f5Kbno8SAF2rW', 'USER'); /* password = 12345zxC! */
 ;
